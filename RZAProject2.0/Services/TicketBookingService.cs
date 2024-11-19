79a366dc1bb786c3e985ace8bf8b8ef24a488184
Finn Parsons <fparso13@gmail.com>
2024-11-19 09:45:24 +0000

    added new ticket and ticketbooking code

---
+++ RZAProject2.0/Services/TicketBookingService.cs
@@ -0,0 +1,23 @@
+﻿using RZAProject2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RZAProject2.Services
+{
+    public class TicketbookingService
+    {
+        private readonly TlS2302452RzaContext _context;
+        public TicketbookingService(TlS2302452RzaContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Ticketbooking>> GetTicketbookingsAsync()
+        {
+            return await _context.Ticketbookings.ToListAsync();
+        }
+        public async Task AddTicketbookingAsync(Ticketbooking newTicketbooking)
+        {
+            await _context.Ticketbookings.AddAsync(newTicketbooking);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ RZAProject2.0/Services/TicketService.cs
@@ -0,0 +1,19 @@
+﻿using RZAProject2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RZAProject2.Services
+{
+    public class TicketService
+    {
+        private readonly TlS2302452RzaContext _context;
+        public TicketService(TlS2302452RzaContext context)
+        {
+            _context = context;
+        }
+        public async Task AddTicketAsync(Ticket newTicket)
+        {
+            await _context.Tickets.AddAsync(newTicket);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
